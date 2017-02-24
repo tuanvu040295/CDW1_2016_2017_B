899daafeab6192e24d7779b5249b67d7b399bc8e
lathanhnhanlk <lathanhnhanlk@gmail.com>
2017-02-24 16:02:36 +0700

    module  516 & 517

--- Blocks/517/js/scripts.js
+++ Blocks/517/js/scripts.js
@@ -74,23 +74,23 @@ $(document).ready(function () {
             }
     );
 });
-$(document).ready(function(){
- 
-      var count_banner = 4,
-      timmer_banner = null;
+$(document).ready(function () {
+
+    var count_banner = 4,
+            timmer_banner = null;
     $('.slogan').animate({
-      'opacity':1
-    },5000);
-    timmer_banner = setInterval(function(){
- 
-      if(count_banner < 0){
-        clearInterval(timmer_banner);
-        return false;
-      }
- 
+        'opacity': 1
+    }, 5000);
+    timmer_banner = setInterval(function () {
+
+        if (count_banner < 0) {
+            clearInterval(timmer_banner);
+            return false;
+        }
+
         $('.banner .photo-' + (count_banner)).addClass('active');
- 
-      count_banner--;
-    },600);
- 
-    });
+
+        count_banner--;
+    }, 600);
+
+});
