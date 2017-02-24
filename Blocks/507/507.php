815b980764aaf95a57918ba0eadfac35b8069913
ltad <nhozghost@gmail.com>
2017-02-24 10:26:39 +0700

    CAP NHAT 507

---
+++ Blocks/507/507.php
@@ -0,0 +1,185 @@
+<!DOCTYPE html>
+
+<html>
+
+    <head>
+        <title></title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <link href="css/507.css" rel="stylesheet" type="text/css" />
+        <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+
+        <?php
+        if (!class_exists('lessc')) {
+            include ('./libs/lessc.inc.php');
+        }
+        $less = new lessc;
+        $less->compileFile('less/507.less', 'css/507.css');
+        ?>
+
+        <script src="js/jquery-3.1.1.min.js" type="text/javascript"></script>
+        <script src="js/scripts.js" type="text/javascript"></script>
+    </head>
+
+    <body>
+            <div class="type-507">
+                <div class="gray-area">
+                    <div class="container">
+                        <div class="row">
+
+                            <div class="col-md-7">
+                                <div class="moduletable" >
+                                    <div class="bghelper">
+
+                                        <h4 class="moduletitle m_title">WELCOME TO KALLYAS TEMPLATE</h4>
+                                        <div class="modulecontent">
+
+
+                                            <div class="custom ">
+                                                <p>
+                                                    <a class="hoverBorder alignright" href="#"> 
+                                                        <span class="hoverBorderWrapper">
+                                                            <img src="images/img6.jpg" alt="">
+                                                            <span class="theHoverBorder"></span>
+                                                        </span>
+                                                    </a>
+                                                </p>
+                                                <p>Vivamus ut molestie nulla. Aenean vel tortor velit. Morbi quis varius tortor. Quisque sit amet leo purus, eget accumsan lorem.<br> Curabitur fringilla ipsum id quam lacinia venenatis.</p>
+                                                <p>Nullam facilisis mattis lacus, ut dapibus risus congue vitae. Suspendisse potenti.</p>
+                                                <p>Nunc sodales dolor at justo posuere vitae pretium magna rhoncus. Sed quis velit eget diam condimentum tempus.</p>
+                                            </div>
+                                        </div><!-- end modulecontent-->
+                                    </div><!-- end bghelper -->
+                                </div><!-- end module -->
+                            </div>
+
+                            <div class="col-md-5">
+                                <div class="moduletable" >
+                                    <div class="bghelper">
+
+                                        <h4 class="moduletitle m_title">TEXT ACCORDION</h4>
+                                        <div class="modulecontent">
+
+
+                                            <div>
+
+                                                <div class="acc-group default-style ">
+                                                    <button data-toggle="collapse" data-target="#acc1923951340" class="collapsed">Vivamus ut molestie nulla. Aenean vel tortor velit.</button>
+                                                    <div id="acc1923951340" class="collapse" style="height: 0px;">
+                                                        <div class="content">
+                                                            Vivamus ut molestie nulla. Aenean vel tortor velit. Morbi quis varius tortor. Quisque sit amet leo purus, eget accumsan lorem.&nbsp;Curabitur fringilla ipsum id quam lacinia venenatis.
+                                                        </div><!-- end content -->
+                                                    </div>
+                                                </div><!-- end acc group -->
+
+
+                                                <div class="acc-group default-style ">
+                                                    <button data-toggle="collapse" data-target="#acc1221990565" class="collapsed ">Duis aute irure dolor in reprehenderit in voluptate.</button>
+                                                    <div id="acc1221990565" class="collapse ">
+                                                        <div class="content">
+                                                            Vivamus ut molestie nulla. Aenean vel tortor velit. Morbi quis varius tortor. Quisque sit amet leo purus, eget accumsan lorem.&nbsp;Curabitur fringilla ipsum id quam lacinia venenatis.
+                                                        </div><!-- end content -->
+                                                    </div>
+                                                </div><!-- end acc group -->
+
+
+                                                <div class="acc-group default-style ">
+                                                    <button data-toggle="collapse" data-target="#acc655589955" class="collapsed ">Neque porro quisquam est qui dolorem.</button>
+                                                    <div id="acc655589955" class="collapse ">
+                                                        <div class="content">
+                                                            Vivamus ut molestie nulla. Aenean vel tortor velit. Morbi quis varius tortor. Quisque sit amet leo purus, eget accumsan lorem.&nbsp;Curabitur fringilla ipsum id quam lacinia venenatis.
+                                                        </div><!-- end content -->
+                                                    </div>
+                                                </div><!-- end acc group -->
+
+
+                                                <div class="acc-group default-style ">
+                                                    <button data-toggle="collapse" data-target="#acc696800979" class="collapsed ">At vero eos et accusamus et iusto odio dignissimos.</button>
+                                                    <div id="acc696800979" class="collapse ">
+                                                        <div class="content">
+                                                            Vivamus ut molestie nulla. Aenean vel tortor velit. Morbi quis varius tortor. Quisque sit amet leo purus, eget accumsan lorem.&nbsp;Curabitur fringilla ipsum id quam lacinia venenatis.
+                                                        </div><!-- end content -->
+                                                    </div>
+                                                </div><!-- end acc group -->
+
+
+                                                <div class="acc-group default-style ">
+                                                    <button data-toggle="collapse" data-target="#acc2134174278" class="collapsed ">Curabitur fringilla ipsum id quam lacinia venenatis.</button>
+                                                    <div id="acc2134174278" class="collapse ">
+                                                        <div class="content">
+                                                            Vivamus ut molestie nulla. Aenean vel tortor velit. Morbi quis varius tortor. Quisque sit amet leo purus, eget accumsan lorem.&nbsp;Curabitur fringilla ipsum id quam lacinia venenatis.
+                                                        </div><!-- end content -->
+                                                    </div>
+                                                </div><!-- end acc group -->
+
+
+                                                <div class="acc-group default-style ">
+                                                    <button data-toggle="collapse" data-target="#acc236932403" class="collapsed ">Duis aute irure dolor in reprehenderit in voluptate.</button>
+                                                    <div id="acc236932403" class="collapse ">
+                                                        <div class="content">
+                                                            Vivamus ut molestie nulla. Aenean vel tortor velit. Morbi quis varius tortor. Quisque sit amet leo purus, eget accumsan lorem.&nbsp;Curabitur fringilla ipsum id quam lacinia venenatis.
+                                                        </div><!-- end content -->
+                                                    </div>
+                                                </div><!-- end acc group -->
+
+                                            </div>
+                                        </div><!-- end modulecontent-->
+                                    </div><!-- end bghelper -->
+                                </div><!-- end module -->
+                            </div>
+
+                        </div>
+                    </div><!-- end #user11 -->
+
+                    <div class="container">
+                        <div class="row ">
+
+                            <div class="col-md-4">
+                                <div class="moduletable">
+                                    <div class="bghelper">
+
+
+                                        <div class="modulecontent">
+
+                                            <div class="testimonial_box " data-align="left" data-size="full" data-theme="light">
+                                                <div class="details">
+                                                    <img src="images/testimonial_logo.png" alt="">
+                                                    <h6><strong>Paul Simon</strong> General Manager</h6>
+                                                </div>
+                                                <blockquote>I'm very happy with your services. I have an excellent website built from scratch and I'm 100% happy.</blockquote>
+                                            </div><!-- end testimonial box -->
+
+                                        </div><!-- end modulecontent-->
+                                    </div><!-- end bghelper -->
+                                </div><!-- end module -->
+                            </div>
+
+                            <div class="col-md-8">
+                                <div class="moduletable">
+                                    <div class="bghelper">
+
+
+                                        <div class="modulecontent">
+
+                                            <div class="testimonial_box " data-align="top" data-size="full" data-theme="dark">
+                                                <div class="details">
+                                                    <img src="images/testimonial_logo.png" alt="">
+                                                    <h6><strong>Andrew Smith</strong> Strategic Coordinator</h6>
+                                                </div>
+                                                <blockquote>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum. Donec id elit non mi porta gravida at eget metus.</blockquote>
+                                            </div><!-- end testimonial box -->
+
+                                        </div><!-- end modulecontent-->
+                                    </div><!-- end bghelper -->
+                                </div><!-- end module -->
+                            </div>
+
+                        </div>
+                    </div><!-- end #user12 -->
+                </div>
+            </div>
+        <script src="js/bootstrap.min.js" type="text/javascript"></script>
+    </body>
+</html>
