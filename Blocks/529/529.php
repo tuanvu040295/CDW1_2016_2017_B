b1c612391cc28c279aad79404681f168bb7de6a6
lathanhnhanlk <lathanhnhanlk@gmail.com>
2017-03-03 10:02:44 +0700

    update module 529

--- Blocks/529/529.php
+++ Blocks/529/529.php
@@ -1,40 +1,33 @@
 <!DOCTYPE html>
 <html>
-
     <head>
         <title>TODO supply a title</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
         <link href="css/styles.css" rel="stylesheet" type="text/css" />
         <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css" />
         <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
-
         <?php
         if (!class_exists('lessc')) {
             include ('libs/lessc.inc.php');
         }
         $less = new lessc;
-        $less->compileFile('less/styles.less', 'css/styles.css');
+        $less->compileFile('less/529.less', 'css/styles.css');
         ?>
         <script src="js/jquery-2.1.4.min.js" type="text/javascript"></script>
         <script src="js/bootstrap.min.js" type="text/javascript"></script>
-        <script src="js/npm.js" type="text/javascript"></script>
-
     </head>
-
     <body>
-        <div class="type-23-f-main">
-            <div class="container">
+        <div class="type-529">
+            <div class="container type-529 footer">
                 <!--Footer Chitiet-->
-                <div class="footer-chitiet">
-                    <div class="border-manhphuc">
-                        <div class="row">
+                <footer class="footer-chitiet">
+                        <section class="row">
                             <!--LEFT-->
-                            <div class="footer-widgets-area with-sidebar-right col-md-6">
+                            <div class="col-md-6">
                                 <!--1-->
                                 <div id="nav_menu-2" class="widget-1 widget-first widget span2 widget_nav_menu col-md-4">
-                                    <h3>Utilities</h3>
+                                    <strong>Utilities</strong>
                                     <ul id="menu-footer" class="menu">
                                         <li class="cols-2"><a href="#">Home</a></li>
                                         <li class="cols-2"><a href="#">Contact</a></li>
@@ -53,71 +46,63 @@
                                     </ul>
                                 </div>
                                 <!--2-->
-                                <div id="woocommerce_products-2" class="widget-2 widget span2 woocommerce widget_products col-md-4">
-                                    <h3>ON SALE</h3>
+                                <div id="woocommerce_products-2" class="col-md-4">
+                                    <strong>ON SALE</strong>
                                     <ul class="product_list_widget">
                                         <li>
-                                            <a href="#" title="Single bed"><img id="sale-manhphuc" src="images/ft-img1.jpg" alt="products" class="img-responsive"><span class="product-title">Single bed</span></a>
+                                            <figure><a href="#" title="Single bed"><img id="sale-manhphuc" src="images/ft-img1.jpg" alt="products" class="img-responsive"><strong class="product-title">Single bed</strong></a></figure>
                                             <del><span class="woocommerce-Price-amount amount"><span class="woocommerce-Price-currencySymbol">£</span>400.00</span></del>
                                             <ins><span class="woocommerce-Price-amount amount"><span class="woocommerce-Price-currencySymbol">£</span>375.00</span></ins>
                                         </li>
-
                                         <li>
-                                            <a href="#" title="Romantic bed"><img id="sale-manhphuc" src="images/ft-img2.jpg" alt="products-2" class="img-responsive"><span class="product-title">Romantic bed</span></a>
+                                            <figure><a href="#" title="Romantic bed"><img id="sale-manhphuc" src="images/ft-img2.jpg" alt="products-2" class="img-responsive"><strong class="product-title">Romantic bed</strong></a></figure>
                                             <del><span class="woocommerce-Price-amount amount"><span class="woocommerce-Price-currencySymbol">£</span>700.00</span></del>
                                             <ins><span class="woocommerce-Price-amount amount"><span class="woocommerce-Price-currencySymbol">£</span>620.00</span></ins>
                                         </li>
                                     </ul>
                                 </div>
                                 <!--3-->
-                                <div id="woocommerce_products-3" class="widget-3 widget-last widget span2 woocommerce widget_products col-md-4">
-                                    <h3>Featured products</h3>
+                                <div id="woocommerce_products-3" class="col-md-4">
+                                    <strong>Featured products</strong>
                                     <ul class="product_list_widget">
                                         <li>
-                                            <a href="#" title="Wood bed"><img id="sale-manhphuc" src="images/ft-img3.jpg" alt="manhphuc-img" class="img-responsive"><span class="product-title">Wood bed</span></a>
+                                            <figure><a href="#" title="Wood bed"><img id="sale-manhphuc" src="images/ft-img3.jpg" alt="manhphuc-img" class="img-responsive"><strong class="product-title">Wood bed</strong></a></figure>
                                             <span class="woocommerce-Price-amount amount"><span class="woocommerce-Price-currencySymbol">£</span>420.00</span>
                                         </li>
-
                                         <li>
-                                            <a href="#" title="Single bed"><img id="sale-manhphuc" src="images/ft-img4.jpg" title="manhphuc-img4" class="img-responsive"><span class="product-title">Single bed</span></a>
+                                            <figure><a href="#" title="Single bed"><img id="sale-manhphuc" src="images/ft-img4.jpg" title="manhphuc-img4" class="img-responsive"><strong class="product-title">Single bed</strong></a></figure>
                                             <del><span class="woocommerce-Price-amount amount"><span class="woocommerce-Price-currencySymbol">£</span>400.00</span></del>
                                             <ins><span class="woocommerce-Price-amount amount"><span class="woocommerce-Price-currencySymbol">£</span>375.00</span></ins>
                                         </li>
                                     </ul>
                                 </div>
-
                             </div>
                             <!--END LEFT-->
-
                             <!--RIGHT-->
-                            <div class="footer-widgets-sidebar with-sidebar-right col-md-6">
+                            <div class="col-md-6">
                                 <div id="footer-recent-posts-2" class="widget-1 widget-first widget span6 recent-posts">
-                                    <h3>Last news</h3>
+                                    <strong>Last news</strong>
                                     <div class="recent-post">
                                         <div class="hentry">
-                                            <div class="text">
-                                                <h3><a href="#">Section shortcodes & sticky posts!</a></h3>
+                                            <article class="text">
+                                                <a href="#">Section shortcodes & sticky posts!</a>
                                                 <p>Fusce nec accumsan eros. Aenean ac orci a magna vestibulum posuere quis nec nisi. Maecenas rutrum vehicula condimentum. Donec volutpat […]</p>
-                                            </div>
+                                            </article>
                                         </div>
-
                                         <div class="hentry">
-                                            <div class="text">
-                                                <h3><a href="#">Another theme by YIThemes!</a></h3>
+                                            <article class="text">
+                                                <a href="#">Another theme by YIThemes!</a>
                                                 <p>Quisque pharetra, risus sit amet vestibulum consequat, elit arcu ultrices sem, eget fringilla felis magna a ante. Ut non quam […]</p>
-                                            </div>
+                                            </article>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                             <!--END RIGHT-->
-                        </div>
-                    </div>
-                </div>
+                        </section>
+                </footer>
                 <!--End Footer Chitiet-->
             </div>
         </div>
-        <script src="js/menu.js" type="text/javascript"></script>
     </body>
-
 </html>
