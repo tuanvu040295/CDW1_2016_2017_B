ffe551cd11213478138e0a02a8ec15de0310aa72
ltad <nhozghost@gmail.com>
2017-03-03 11:01:39 +0700

    cap nhat 527

---
+++ Blocks/527/527.php
@@ -0,0 +1,161 @@
+<html>
+
+    <head
+        <title></title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+        <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+
+        <script src="js/jquery-2.1.4.min.js" type="text/javascript"></script>
+        <?php
+        if (!class_exists('lessc')) {
+            include ('./libs/lessc.inc.php');
+        }
+        $less = new lessc;
+        $less->compileFile('./less/527.less', './css/527.css');
+        ?>
+        <link href="css/527.css" rel="stylesheet" type="text/css" />
+
+    </head>
+
+    <body>
+
+        <div class="type-527">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="col-md-9 col-sm-9 col-xs-12">
+                            <div class="row-9">
+                                <div class="col-md-12">
+                                    <div class="revolution-slider" class="slider revolution-slider alignleft">
+                                        <div id="w_2_1_wrapper" >
+                                            <!-- START REVOLUTION SLIDER 5.2.5.1 responsitive mode -->
+                                            <div id="w_2_1" >
+                                                <ul>
+                                                    <!-- SLIDE -->
+                                                    <li>
+                                                        <!-- MAIN IMAGE -->
+                                                        <div class="item active" class="slotholder"  >
+                                                            <img src="images/1-bg1.jpg" >
+                                                        </div>
+                                                    <li>
+                                                        <!--MAIN IMAGE -->
+                                                        <div class="slotholder" >
+                                                            <img src="images/03-bg.jpg">
+                                                        </div>
+                                                    </li>
+                                                    <div class="check1">
+                                                        <a class="i-left12" href="#" > <i class="fa fa-chevron-circle-left fa-2x" ></i> </a>
+                                                        <a class="i-right12" href="#" > <i class="fa fa-chevron-circle-right fa-2x" ></i></a>
+                                                    </div>   
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="text">
+                                        <h2>Fall in love with Cheope</h2>
+                                        <h5>a complete solution for your shop</h5>
+                                        <p>Donec elit felis, scelerisque eu mattis rhoncus, luctus quis magna. Suspendisse ligula nunc, aliquam auctor volutpat a, pretium vitae neque. Mauris odio est, varius eu mollis et, dapibus sed massa. Integer eu tellus ac diam aliquet dictum. Aenean quis sagittis augue.</p>
+                                        <p>Suspendisse ligula nunc, aliquam auctor volutpat a, pretium vitae neque.&nbsp;Quisque consectetur turpis at metus venenatis vitae lobortis lectus accumsan. Aliquam erat volutpat.</p>
+                                    </div>
+                                    <div class="khoangcach"> </div>
+
+                                    <div class="one-third ">
+                                        <div class="sc-banner" id="banner0" >
+                                            <a href="#">
+                                                <i class="fa fa-phone" aria-hidden="true"></i>
+                                                <div class="sc-content">
+                                                    <h2>CUSTOMER SUPPORT</h2>
+                                                    <h3>Call us +00 39 837372</h3>
+                                                </div>
+                                            </a>
+                                        </div>
+                                    </div>
+
+                                    <div class="one-third">
+                                        <div class="sc-banner" id="banner0" >
+                                            <a href="#">
+                                                <i class="fa fa-comments" aria-hidden="true"></i>
+                                                <div class="sc-content">
+                                                    <h2>HAPPY CUSTOMERS</h2>
+                                                    <h3>Read the testimonials</h3>
+                                                </div>
+                                            </a>
+                                        </div>
+
+                                    </div>
+                                    <div class="one-third">
+                                        <div class="sc-banner" id="banner0" >
+                                            <a href="#">
+                                                <i class="fa fa-bullhorn" aria-hidden="true"></i>
+                                                <div class="sc-content">
+                                                    <h2>HAPPY CUSTOMERS</h2>
+                                                    <h3>Read the testimonials</h3>
+                                                </div>
+                                            </a>
+                                        </div>
+                                    </div>
+
+                                </div>
+
+                            </div>
+
+                        </div>
+
+                        <div class="row-3">
+                            <h4>Must Have</h4>
+                            <div class="col-md-4">
+
+
+                                <div  class="carousel slide">
+                                    <div class="carousel-inner">
+                                        <div class="item active" >
+                                            <img  src="images/0159186_PE315624_S5-copia-160x160.jpg" alt="" />
+                                            <ul>
+                                                <li class="uiliti li">
+                                                    <a href="#">Wood bed</a><br>
+                                                    <span>£420.00</span>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </div>
+
+                                    <a class="left carousel-control" href="#myCarousel" >
+                                        <span <i class="glyphicon fa fa-chevron-left" aria-hidden="true" ></i></span>
+                                        <span class="sr-only">Previous</span>
+                                    </a>
+                                    <a class="right carousel-control" href="#myCarousel">
+                                        <span <i class="glyphicon fa fa-chevron-right" aria-hidden="true" ></i></span>
+                                        <span class="sr-only">Next</span>
+                                    </a>
+                                </div>
+                            </div>
+
+                            <h4 class="tesa">Testimonials</h4>
+                            <div class="col-md-4">
+                                <div class="slide">
+
+                                    <div class="carousel-inner" >
+                                        <div class="item active" >
+                                            <li class="li">
+                                                <div class="commnent ">
+                                                    <p>Just want to give some feedback. I have purchased many themes from here with different levels of support but have come across none whose responses […]</p>
+                                                </div>
+                                                <img width="35" height="35" src="images/due-35x35.jpg">
+                                                <div class="name-testimonial"><a class="name-testimonial" href="#">Lady Harvey</a><br>
+                                                    <span class="label-testimonial" >SEO of Apple Inc</span>
+                                                </div>
+                                            </li>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
