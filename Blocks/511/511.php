e5f6113c00ff516afcb9c3d0d332958488ba0c0d
tuanvu040295 <tuanvunemo@gmail.com>
2017-03-10 12:19:26 +0700

    Module 11

--- Blocks/511/511.php
+++ Blocks/511/511.php
@@ -2,54 +2,44 @@
 
 <html>
 
-<head>
-    <title></title>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <head>
+        <title></title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css" />
-    <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
-    <?php
-    if (!class_exists('lessc')) {
-        include ('./libs/lessc.inc.php');
-    }
-    $less = new lessc;
-    $less->compileFile('less/511.less', 'css/511.css');
-    ?>
-    <link href="css/511.css" rel="stylesheet" type="text/css" />
+        <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+        <?php
+        if (!class_exists('lessc')) {
+            include ('./libs/lessc.inc.php');
+        }
+        $less = new lessc;
+        $less->compileFile('less/511.less', 'css/511.css');
+        ?>
+        <link href="css/511.css" rel="stylesheet" type="text/css" />
 
-
-    <script src="js/jquery-3.1.1.min.js" type="text/javascript"></script>
-    <script src="js/scripts.js" type="text/javascript"></script>
-
-</head>
-
-<body>
-    <div class="type-511">
-        <div class="container type-511-content">
-            <div class="row">
-                <!--BEGIN PREFERENTIAL BANNER-->
-                <section class="col-sm-12">
-                    <h4 class="title">SOME OF OUR LATEST OFFERS</h4>
-                    <!-- BEGIN IMAGES-->
+        <script src="js/jquery-3.1.1.min.js" type="text/javascript"></script>
+    </head>
+    <body>
+        <div class="type-511">
+            <div class="container">
+                <section >
                     <div class="row">
-                        <figure class="col-sm-4">
-                            <a class="hoverborder" href="#"><span><img src="images/bnr1.jpg" alt=""><span class="theHoverBorder"></span></span></a>
-                        </figure>
-                        <figure class="col-sm-5">
-                            <a class="hoverborder" href="#"><span><img src="images/bnr2.jpg" alt=""><span class="theHoverBorder"></span></span></a>
-                        </figure>
-                        <figure class="col-sm-3">
-                            <a class="hoverborder" href="#"><span><img src="images/bnr3.jpg" alt=""><span class="theHoverBorder"></span></span></a> 
-                            <a class="hoverborder" href="#"><span><img src="images/bnr4.jpg" alt=""><span class="theHoverBorder"></span></span></a>
-                        </figure>                      
+                        <div class="col-sm-4">
+                            <a href="#" class=" -link hoverBorder" target="_blank" itemprop="url"><img src="images/bnr1.jpg" alt="" title="banner-set1-1.jpg" class="img-responsive"></a>
+                        </div>
+                        <div class="col-sm-5">
+                            <a href="#" class=" -link hoverBorder" target="_blank" itemprop="url"><img src="images/bnr2.jpg" alt="" title="banner-set1-2.jpg" class="img-responsive"></a>
+                        </div>
+                        <div class="col-sm-3">
+                            <a href="#" class=" -link hoverBorder" target="_blank" itemprop="url"><img src="images/bnr3.jpg" alt="" title="banner-set2-32.jpg" class="img-responsive"></a>
+                        </div>
+                        <div class="col-sm-3">
+                            <a href="#" class=" -link hoverBorder" target="_blank" itemprop="url"><img src="images/bnr4.jpg" alt="" title="banner-set2-41.jpg" class="img-responsive"></a>
+                        </div>
                     </div>
-                    <!--END IMAGES--> 
                 </section>
-                <!--END PREFERENTIAL BANNER-->
             </div>
         </div>
-    </div>
-    <script src="js/bootstrap.min.js" type="text/javascript"></script>
-</body>
+    </body>
 </html>
