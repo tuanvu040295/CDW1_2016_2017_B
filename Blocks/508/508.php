d5348ed7b0eb8ed0eb5d842e16950c1eca3911b7
ltad <nhozghost@gmail.com>
2017-03-17 11:13:44 +0700

    508 OK

--- Blocks/508/508.php
+++ Blocks/508/508.php
@@ -27,62 +27,64 @@
         <div class="type-508">
             <div class="container type-508-content ">
                 <!-- BEGIN MODULE 508-->
-
-                <div class="row">
-                    <!-- BEGIN MODULECONTENT-->
-                    <div class="process_steps ">
-                        <div class="col-md-3">
-                            <div class="step intro">
-                                <h3>our working process 
-                                    <strong>in 3 steps</strong>
-                                </h3>
-                                <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi.</p>
-                                <p style="margin-bottom: 0;">
-                                    <a href="#">MORE ABOUT US +</a>
-                                </p>
+                <div class="col-md-12 col-sm-12">
+                    <div class="row ">
+                        <!-- BEGIN MODULECONTENT-->
+                        <div class="process_steps ">
+                            <!-- BEGIN STEP INTRO -->
+                            <div class="col-md-3 khoangcach" >
+                                <div class="step intro">
+                                    <h3>our working process 
+                                        <strong>in 3 steps</strong>
+                                    </h3>
+                                    <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi.</p>
+                                    <p style="margin-bottom: 0;">
+                                        <a href="#">MORE ABOUT US +</a>
+                                    </p>
+                                </div>
                             </div>
-                        </div>
-                        <!-- BEGIN STEP 1 -->
-                        <div class="col-md-3"> 
-                            <div class="step step1 ">
-                                <figure class="icon" data-animation="tada">
-                                    <img src="images/ico-plan.png" alt="planning & strategy">
-                                </figure>
-                                <h3>planning & strategy</h3>
-                                <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.</p>
+                            <!-- END STEP INTRO -->
 
+
+                            <!-- BEGIN STEP 1 -->
+                            <div class="col-md-3 khoangcach"> 
+                                <div class="step step1 ">
+                                    <figure class="icon" data-animation="tada">
+                                        <img src="images/ico-plan.png" alt="planning & strategy">
+                                    </figure>
+                                    <h3>planning & strategy</h3>
+                                    <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.</p>
+                                </div>
                             </div>
+                            <!-- END STEP 1 -->
 
-                        </div>
-                        <!-- END STEP 1 -->
+                            <!-- BEGIN STEP 2 -->
+                            <div class="col-md-3 khoangcach">
+                                <div class="step step2 ">
+                                    <figure class="icon" data-animation="pulse">
+                                        <img src="images/ico-designdev.png" alt="design & develop">
+                                    </figure>
+                                    <h3>design & develop</h3>
+                                    <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.</p>
+                                </div>
+                            </div
+                            <!-- END STEP 2 -->
 
-                        <!-- BEGIN STEP 2 -->
-                        <div class="col-md-3">
-                            <div class="step step2 ">
-                                <figure class="icon" data-animation="pulse">
-                                    <img src="images/ico-designdev.png" alt="design & develop">
-                                </figure>
-                                <h3>design & develop</h3>
-                                <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.</p>
+                            <!-- BEGIN STEP 3 -->
+                            <div class="col-md-3 khoangcach">
+                                <div class="step step3">
+                                    <figure class="icon" data-animation="fadeOutRightBig">
+                                        <a href="#" target="_self">
+                                            <img src="images/ico-launch.png" alt="test & deliver">
+                                        </a>
+                                    </figure>
+                                    <h3>test & deliver</h3>
+                                    <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.</p>
+                                </div>
                             </div>
-                        </div
-                        <!-- END STEP 2 -->
-
-                        <!-- BEGIN STEP 3 -->
-                        <div class="col-md-3">
-                            <div class="step step3">
-                                <figure class="icon" data-animation="fadeOutRightBig">
-                                    <a href="#" target="_self">
-                                        <img src="images/ico-launch.png" alt="test & deliver">
-                                    </a>
-                                </figure>
-                                <h3>test & deliver</h3>
-                                <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.</p>
-                            </div>
-                        </div>
-                        <!-- END STEP 3 -->
-
-                    </div>			
+                            <!-- END STEP 3 -->
+                        </div>			
+                    </div>
                 </div>
                 <!-- END MODULECONTENT-->
             </div>
