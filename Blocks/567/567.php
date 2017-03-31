5201a84b3476c68726b3281842d16cb61221303d
La Thành Nhân <lathanhnhanlk@gmail.com>
2017-03-31 10:01:34 +0700

    Module 567 DONE

--- Blocks/567/567.php
+++ Blocks/567/567.php
@@ -12,20 +12,18 @@
             include ('./libs/lessc.inc.php');
         }
         $less = new lessc;
-        $less->compileFile('less/styles.less', 'css/styles.css');
+        $less->compileFile('less/567.less', 'css/567.css');
         ?>
 
-        <link href="css/styles.css" rel="stylesheet" type="text/css" />
+        <link href="css/567.css" rel="stylesheet" type="text/css" />
         <script src="js/jquery-2.1.4.min.js" type="text/javascript"></script>
         <script src="js/bootstrap.min.js" type="text/javascript"></script>
     </head>
 
     <body>
-        <div class="type_a_2_chitiet_2">
-            <div class="content">
-                <div class="clearfix">
-
-
+        <div class="type-567">
+            <div class="container">
+                <div class="col-md-9">
                     <div class="related_articles clearfix">
                         <div class="related_title">
                             Related Articles
@@ -57,29 +55,19 @@
                                             <a rel="nofollow" class="comment-reply-link" href="#">Reply</a>
                                             <img src="images/images2.jpg" alt=""/>
                                             <div >
-
-                                                <span class="fn">demouser<span class="rh-user-rank-mc">Newbie
-                                                    </span>
+                                                <span class="fn">demouser
+                                                    <span class="rh-user-rank-mc">Newbie</span>
                                                 </span>
-
-                                                </span>
-
                                                 <div>
                                                     <i class="fa fa-star-o">
                                                         0.00
                                                     </i>
                                                 </div>
-
-
                                                 <span>April 11, 2016 at 12:55 pm</span>
-
                                             </div>
                                         </div>
-
                                         <p>This is excellent deal, bro
                                         </p>
-
-
                                     </div>
                                 </li><!-- #comment-## -->
                                 <li class="comment odd alt" >
@@ -88,22 +76,14 @@
                                             <a rel="nofollow" class="comment-reply-link" href="#"> Reply</a>
                                             <img src="images/images2.jpg" alt=""/>
                                             <div >
-
                                                 <span class="fn">demouser<span class="rh-user-rank-mc">Newbie
                                                     </span>
-
                                                 </span>
-
                                                 <div><i class="fa fa-star-o"></i> 0.00</div>
-
-
                                                 <span class="time">April 11, 2016 at 12:55 pm</span>
-
                                             </div>
                                         </div>
                                         <p>So pretty thing</p>
-
-
                                     </div>
                                 </li><!-- #comment-## -->
                                 <li class="comment even" >
@@ -112,18 +92,13 @@
                                             <a rel="nofollow" class="comment-reply-link" href="#" >Reply</a>
                                             <img src="images/images2.jpg" alt=""/>
                                             <div >
-
                                                 <span class="fn">nanetest</span>
                                                 <div >
                                                 </div>
-
                                                 <span class="time">May 10, 2016 at 6:58 am</span>
-
                                             </div>
                                         </div>
                                         <p>this is a test comment</p>
-
-
                                     </div>
                                 </li><!-- #comment-## -->
                                 <li class="comment odd alt" id=" 4">
@@ -132,22 +107,14 @@
                                             <a rel="nofollow" class="comment-reply-link" href="#">Reply</a>
                                             <img src="images/images2.jpg" alt=""/>
                                             <div >
-
                                                 <span class="fn">bill<span class="rh-user-rank-mc">Newbie
                                                     </span>
-
                                                 </span>
-
                                                 <div><i class="fa fa-star-o"></i> 0.00</div>
-
-
                                                 <span class="time">May 27, 2016 at 10:55 am</span>
-
                                             </div>
                                         </div>
                                         <p>O M G</p>
-
-
                                     </div>
                                 </li><!-- #comment-## -->
                                 <li class="comment even" >
@@ -156,26 +123,18 @@
                                             <a rel="nofollow" class="comment-reply-link" href="#">Reply</a>
                                             <img src="images/images2.jpg" alt=""/>
                                             <div >
-
                                                 <span class="fn"><a href="#">dkssud
                                                     </a>
                                                 </span>
-
-
                                                 <div>
                                                     <i class="fa fa-star-o">
                                                         15.00
                                                     </i>
                                                 </div>
-
-
                                                 <span class="time">September 3, 2016 at 5:36 am</span>
-
                                             </div>
                                         </div>
                                         <p>Test</p>
-
-
                                     </div>
                                 </li><!-- #comment-## -->
                                 <li class="comment odd alt" >
@@ -184,33 +143,26 @@
                                             <a rel="nofollow" class="comment-reply-link" href="#">Reply</a>
                                             <img src="images/images2.jpg" alt=""/>
                                             <div >
-
                                                 <span class="fn">
                                                     <a href="#S" target="_blank" rel="nofollow">testor
                                                     </a>
                                                 </span>
                                                 <div >
                                                 </div>
-
                                                 <span class="time">September 11, 2016 at 9:32 pm</span>
-
                                             </div>
                                         </div>
                                         <p>testor</p>
-
-
                                     </div>
                                 </li><!-- #comment-## -->
                             </ol>
                             <div >
                             </div>
                         </div>
-
                         <ol class="commentlist">
                         </ol>
                         <div class="comment-">
                             <h3 >Leave a reply
-
                             </h3>
                             <form action="#" method="post" >
                                 <textarea id="comment" name="comment" cols="30" rows="10" aria-required="true">
@@ -222,9 +174,7 @@
                                         </div>
                                     </div>
                                     <div class="col-md-4">
-
                                         <input type="text" value="" name="" placeholder="E-mail">
-
                                     </div>
                                     <div class="col-md-4">
                                         <div class="site_re end">
@@ -233,7 +183,6 @@
                                     </div>
                                 </div>
                                 <div class="clearfix">
-
                                 </div>
                                 <p class="form-submit">
                                     <input name="submit" type="submit" id="submit" class="submit" value="Post comment">
