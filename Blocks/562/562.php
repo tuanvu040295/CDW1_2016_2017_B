853a8be33cdb04b48b5323c8871de52ca7f79ac5
ltad <nhozghost@gmail.com>
2017-03-31 08:13:13 +0700

    module 562 up

--- Blocks/562/562.php
+++ Blocks/562/562.php
@@ -4,7 +4,7 @@ if (!class_exists('lessc')) {
     include ('./libs/lessc.inc.php');
 }
 $less = new lessc;
-$less->compileFile('./less/type-d-12.less', 'css/style.css');
+$less->compileFile('./less/562.less', 'css/562.css');
 ?>
 <html lang="en">
     <head>
@@ -15,18 +15,18 @@ $less->compileFile('./less/type-d-12.less', 'css/style.css');
         <title>Bootstrap 101 Template</title>
         <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
         <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
-        <link href="css/style.css" rel="stylesheet" type="text/css"/>
+        <link href="css/562.css" rel="stylesheet" type="text/css"/>
 
         <script src="js/jquery.min.js" type="text/javascript"></script>
         <script src="js/bootstrap.min.js" type="text/javascript"></script>
 
     </head>
     <body>
-        <div class="mod_k">
-            <div class="container">
-                <div class="row">
-
-                    <div class="col-md-9">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-9">
+                    <div class="type-562">
+                        <!------------BEGIN UPDATES---------->
                         <div class="row ">
                             <div class="col-md-12 header">
                                 <header class="sectionTitle clearfix">
@@ -34,11 +34,18 @@ $less->compileFile('./less/type-d-12.less', 'css/style.css');
                                 </header>
                             </div>
                         </div>
+                        <!------------END UPDATES---------->
+
+                        <!------------BEGIN CONTENT UPDATES---------->
                         <div class="row ">
                             <div class="col-md-12">
-
-                                <p><strong><span class="rbLarge">Update #1</span></strong><br>
-                                    <span class="rbSmall">Mar 27, 2013</span></p>
+                                <p>
+                                    <strong>
+                                        <span class="rbLarge">Update #1</span>
+                                    </strong>
+                                    <br>
+                                    <span class="rbSmall">Mar 27, 2013</span>
+                                </p>
                                 <h3><b>International Shipping</b></h3>
                                 <p class="content">Nullam id dolor id nibh ultricies vehicula ut id elit. Integer posuere erat a ante venenatis dapibus
                                     posuere velit aliquet. Duis mollis, est non commodo luctus. Aenean lacinia bibendum nulla. Donec sed 
@@ -46,18 +53,12 @@ $less->compileFile('./less/type-d-12.less', 'css/style.css');
                                     dapibus posuere velit aliquet. Duis mollis, est non commodo luctus. Aenean lacinia bibendum nulla. Donec 
                                     sed odio dui.
                                 </p>
-
-
                             </div>
                         </div>
-
-                    </div>
-                    <div class="col-md-3">
-                       
+                        <!------------END CONTENT UPDATES---------->
                     </div>
                 </div>
             </div>
-        </div>
-        <script src="js/javascript.js" type="text/javascript"></script>
+            <script src="js/javascript.js" type="text/javascript"></script>
     </body>
 </html>
