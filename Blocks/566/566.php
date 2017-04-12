d63bea1d49c979fc6e270f7449eaf1ee4522d0d7
lathanhnhan <lathanhnhanlkgmail.com>
2017-04-12 14:54:26 +0700

    Module 566

---
+++ Blocks/566/566.php
@@ -0,0 +1,125 @@
+<html>
+
+    <head>
+        <title>TODO supply a title</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="user-scalable=0, initial-scale=1.0">
+        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+        <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+
+
+        <?php
+        if (!class_exists('lessc')) {
+            include ('./libs/lessc.inc.php');
+        }
+        $less = new lessc;
+        $less->compileFile('less/566.less', 'css/566.css');
+        ?>
+        <link href="css/566.css" rel="stylesheet" type="text/css" />
+        <script src="js/jquery-2.1.4.min.js" type="text/javascript"></script>
+        <script src="js/bootstrap.min.js" type="text/javascript"></script>
+    </head>
+
+    <body>
+        <div class="type-566">
+            <div class="col-md-8"></div>
+            <div class="col-md-3 type-566-sidebar">
+                <div class="container">
+                    <div id="rehub_tabsajax_widget-2" class="widget tabsajax"><div class="title">Most hot</div>												
+                        <div class="ajaxed_post_widget rh_col_tabs_3">
+                            <div class="re_filter_panel"><ul class="re_filter_ul"><li><span data-sorttype="{&quot;filtertype&quot;:&quot;meta&quot;,&quot;filterorder&quot;:&quot;DESC&quot;,&quot;filterdate&quot;:&quot;all&quot;,&quot;filtermetakey&quot;:&quot;post_hot_count&quot;}" class="active re_filtersort_btn resort_0" data-containerid="rh_simplepostid_58edd183cc81a">Best rated</span></li><li><span data-sorttype="{&quot;filtertype&quot;:&quot;meta&quot;,&quot;filterorder&quot;:&quot;DESC&quot;,&quot;filterdate&quot;:&quot;month&quot;,&quot;filtermetakey&quot;:&quot;rehub_views&quot;}" class="re_filtersort_btn resort_1" data-containerid="rh_simplepostid_58edd183cc81a">This month</span></li><li><span data-sorttype="{&quot;filtertype&quot;:&quot;meta&quot;,&quot;filterorder&quot;:&quot;DESC&quot;,&quot;filterdate&quot;:&quot;year&quot;,&quot;filtermetakey&quot;:&quot;rehub_views&quot;}" class="re_filtersort_btn resort_2" data-containerid="rh_simplepostid_58edd183cc81a">This year</span></li></ul></div>		<div class="wpsm_recent_posts_list" data-filterargs="{&quot;post_type&quot;:&quot;post&quot;,&quot;posts_per_page&quot;:6,&quot;orderby&quot;:&quot;meta_value_num&quot;,&quot;order&quot;:&quot;DESC&quot;,&quot;meta_key&quot;:&quot;post_hot_count&quot;,&quot;no_found_rows&quot;:1}" data-template="simplepostlist" id="rh_simplepostid_58edd183cc81a" data-innerargs="{&quot;nometa&quot;:&quot;1&quot;,&quot;image&quot;:&quot;&quot;}">
+                                <div class="item-small-news">
+                                    <div class="item-small-news-details">
+                                        <h3><span id="temperatur169" class="temperatur">428<span class="gradus_icon"></span></span> <a href="#">Calcium with Magnesium, Natural Orange Flavor</a>
+
+                                            <span class="simple_price_count">
+                                                $15.36    			 <del>$19.95</del>    		</span>
+                                        </h3>
+                                        <div class="post-meta">
+                                            <span class="date_ago">
+                                                1 year ago	        </span>
+                                            <span class="comm_number_for_list"><i class="fa fa-commenting"></i> 7</span>
+                                        </div> 
+                                    </div>   
+                                </div>			
+                                <div class="item-small-news">
+                                    <div class="item-small-news-details">
+                                        <h3><span id="temperatur159" class="temperatur">367<span class="gradus_icon"></span></span> <a href="#">Apple Pectin, 700 mg</a>
+
+                                            <span class="simple_price_count">
+                                                $10.18    			 <del>$15.99</del>    		</span>
+                                        </h3>
+                                        <div class="post-meta">
+                                            <span class="date_ago">
+                                                1 year ago	        </span>
+                                            <span class="comm_number_for_list"><i class="fa fa-commenting"></i> 1</span>
+                                        </div> 
+                                    </div>   
+                                </div>			
+                                <div class="item-small-news">
+                                    <div class="item-small-news-details">
+                                        <h3><span id="temperatur156" class="temperatur">217<span class="gradus_icon"></span></span> <a href="#">Garlic Oil, 1500 mg</a>
+
+                                            <span class="simple_price_count">
+                                                $8.28    			 <del>$12.99</del>    		</span>
+                                        </h3>
+                                        <div class="post-meta">
+                                            <span class="date_ago">
+                                                1 year ago	        </span>
+                                            <span class="comm_number_for_list"><i class="fa fa-commenting"></i> 0</span>
+                                        </div> 
+                                    </div>   
+                                </div>			
+                                <div class="item-small-news">
+                                    <div class="item-small-news-details">
+                                        <h3><span id="temperatur151" class="temperatur">215<span class="gradus_icon"></span></span> <a href="#">Vitamin C Serum for Face with Hyaluronic Acid</a>
+
+                                            <span class="simple_price_count">
+                                                $15.95     			 <del>$79.95</del>    		</span>
+                                        </h3>
+                                        <div class="post-meta">
+                                            <span class="date_ago">
+                                                1 year ago	        </span>
+                                            <span class="comm_number_for_list"><i class="fa fa-commenting"></i> 1</span>
+                                        </div> 
+                                    </div>   
+                                </div>			
+                                <div class="item-small-news">
+                                    <div class="item-small-news-details">
+                                        <h3><span id="temperatur144" class="temperatur">212<span class="gradus_icon"></span></span> <a href="#">Soft and Ultra fine bristles</a>
+
+                                            <span class="simple_price_count">
+                                                $9.97    			 <del>$19.75</del>    		</span>
+                                        </h3>
+                                        <div class="post-meta">
+                                            <span class="date_ago">
+                                                1 year ago	        </span>
+                                            <span class="comm_number_for_list"><i class="fa fa-commenting"></i> 3</span>
+                                        </div> 
+                                    </div>   
+                                </div>			
+                                <div class="item-small-news">
+                                    <div class="item-small-news-details">
+                                        <h3><span id="temperatur124" class="temperatur">189<span class="gradus_icon"></span></span> <a href="#">Plant-Based Daily Superfood + Probiotics and Digestive Enzymes</a>
+
+                                            <span class="simple_price_count">
+                                                $64.56    			 <del>$67.96</del>    		</span>
+                                        </h3>
+                                        <div class="post-meta">
+                                            <span class="date_ago">
+                                                1 year ago	        </span>
+                                            <span class="comm_number_for_list"><i class="fa fa-commenting"></i> 2</span>
+                                        </div> 
+
+                                    </div>   
+                                </div>		
+                            </div>
+                            <div class="clearfix"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+
+</html>
