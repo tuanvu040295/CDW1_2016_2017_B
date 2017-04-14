255d34afb120452081af91f2e89d5e1bdf8f62d6
tuandat910 <tuandat910>
2017-04-14 09:36:18 +0700

    update 588 again and 589

--- Blocks/588/588.php
+++ Blocks/588/588.php
@@ -1,16 +1,13 @@
 <!DOCTYPE html>
-
 <html>
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
         <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
         <script src="js/jquery-3.1.1.min.js" type="text/javascript"></script>
         <script src="js/bootstrap.min.js" type="text/javascript"></script>
         <script src="js/newjavascript.js" type="text/javascript"></script>
         <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
-
         <?php
         if (!class_exists('lessc')) {
             include ('./libs/lessc.inc.php');
@@ -19,28 +16,20 @@
         $less->compileFile('less/588.less', 'css/588.css');
         ?>
         <link href="css/588.css" rel="stylesheet" type="text/css"/>
-
-
     </head>
     <body>
-        <div class="type-588">
+        <section class="type-588">
             <!--menu-->
-            
             <!--endmenu-->
-
             <!--body-->
-
             <div class="ds">
-                                             
                 <div class="product">
                     <div class="image">
                         <img src="image/img3.jpg" alt=""/>
-
                     </div>
                     <div class="opa"></div>
                     <div class="name">
                         <div class="blue"></div>
-
                         <h2>Vivamus finibus magna nec sed</h2>
                         <span class="big">In tempor sollicitudin sagittis! nullam</span>
                     </div>
@@ -55,7 +44,6 @@
                             <h5 class="label">Client</h5>
                             <h5>Muffin Group</h5>
                         </div>
-
                         <div class="website">
                             <hr>
                             <h5 class="label">Website</h5>
@@ -63,18 +51,13 @@
                         </div>
                     </div>
                 </div>
-
                 <div class="sea">
                     <div class="see">
                         <h1>See it in action <span class="fa fa-angle-right" style="margin-left: 12px;"></span></h1>
                     </div>
                 </div>
-             
             </div>
-
-        </div>
-        <!--endbody-->
-
-    </div>
-</body>
+            <!--endbody-->
+        </section>
+    </body>
 </html>
--- Blocks/589/589.php
+++ Blocks/589/589.php
@@ -1,64 +1,41 @@
 <!DOCTYPE html>
-
 <html>
     <head>
         <meta charset="UTF-8">
-
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-
-
         <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
         <script src="js/jquery-3.1.1.min.js" type="text/javascript"></script>
         <script src="js/bootstrap.min.js" type="text/javascript"></script>
         <script src="js/newjavascript.js" type="text/javascript"></script>
         <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
-
         <?php
         if (!class_exists('lessc')) {
             include ('./libs/lessc.inc.php');
         }
         $less = new lessc;
-        $less->compileFile('less/style1.less', 'css/stylee.css');
+        $less->compileFile('less/589.less', 'css/589.css');
         ?>
-        <link href="css/stylee.css" rel="stylesheet" type="text/css"/>
-
-
+        <link href="css/589.css" rel="stylesheet" type="text/css"/>
     </head>
     <body>
-        <div class="type_43">
+        <section class="type-589">
             <!--menu-->
-            
             <!--endmenu-->
-
             <!--body-->
-
             <div class="ds">
-                
-
-                
-
-                
-
                 <div class="mod-buy">
                     <div class="buy-now">
                         <h2 >Creating with BeTheme is super fast and easy
                             <br>
-                            Start your website today!</h2>
+                            Start your website today!
+                        </h2>
                         <div class="btn-buynow">
                             <a href="#">BUY NOW FOR $59</a>
                         </div>
                     </div>
                 </div>
-                
             </div>
-
-
-        </div>
-        <!--endbody-->
-
-
-
-    </div>
-</body>
+            <!--endbody-->
+        </section>
+    </body>
 </html>
