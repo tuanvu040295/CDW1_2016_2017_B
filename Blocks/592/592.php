5514d4578110eb8ec5e6b3879ef5fbfc9b616bd4
tuanvu040295 <tuanvunemo@gmail.com>
2017-04-19 17:04:49 +0700

    592

--- Blocks/592/592.php
+++ Blocks/592/592.php
@@ -21,51 +21,73 @@
     <body>
         <div class="type-592">
             <div class="container">
-                <div class="col-md-12">
-                    <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-                        <div class="containerCentered">
-                            <h2>Great Versitility Included for Free</h2>
-                            <p class="lead">Click on the Phone Screen</p>	
-                        </div> 
-                        <div class="carousel-inner">
-                            <div class="item active">
-                                <div id="sc-wrapper" class="sc-wrapper">
-                                    <h2 class="sc-title">Superior Range</h2>
-                                    <div class="sc-iphone sc-device">
-                                        <a href="#"><img class="img-responsive" src="image/iPhone.png"></a>
-                                        <span class="sc-text sc-sub">
-                                            <p><span class="firstcharacter">D</span>uis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Etiam porta sem malesuada magna mollis euismod. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam quis risus eget urna mollis ornare vel eu leo. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.</p>
-                                            <p>Integer posuere erat a ante venenatis dapibus posuere velit aliquet. </p>
-                                        </span>
-                                        <nav>
-                                            <a href="#carousel-example-generic" data-slide="prev"><span class="fa fa-chevron-circle-left" aria-hidden="true"></span></a>
-                                            <a href="#carousel-example-generic" data-slide="next"><span class="fa fa-chevron-circle-right" aria-hidden="true"></span></a>
-                                        </nav>
-                                    </div>
+                <!-- BEGIN CONTENT -->
+                <div class="contact">
+                    <div class="col-md-12">
+                        <!-- BEGIN FORM CONTACT -->
+                        <div class="col-md-6">
+                            <h4 class="aq-block-title">Contact Us</h4>
+                            <form>
+                                <p>Your Name (required)<br>
+                                    <span><input type="text" name="your-name" value="" size="40"></span> 
+                                </p>
+                                <p>Your Email (required)<br>
+                                    <span><input type="email" name="your-email" value="" size="40"></span> 
+                                </p>
+                                <p>Subject<br>
+                                    <span><input type="text" name="your-subject" value="" size="40"></span> 
+                                </p>
+                                <p>Your Message<br>
+                                    <span><textarea name="your-message" cols="40" rows="10"></textarea></span> 
+                                </p>
+                                <p><input type="submit" value="Send"></p>
+                            </form>
+                        </div>
+                        <!-- END FORM CONTACT -->
+                        <!-- HAPPY USERS -->
+                        <div class="sc_stats col-md-3">
+                            <i class="fa fa-heart-o" aria-hidden="true"></i>
+                            <div>
+                                <span class="highlight">364</span> 
+                                <div class="milestone-details">
+                                    <p>Happy Users</p>
                                 </div>
                             </div>
-                            <div class="item">
-                                <div id="sc-wrapper" class="sc-wrapper">
-                                    <h2 class="sc-title">Ultimate Flexibility</h2>					
-                                    <div class="sc-iphone sc-device">
-                                        <a href="#"><img src="image/iPhone.png"></a>
-                                        <span class="sc-text sc-sub">
-                                            <p><span class="firstcharacter">A</span><strong>cool way</strong> to show off your <strong>App</strong>.The app showcase is part of the page builder so you can add it anywhere.  Load as many screenshots as you need.</p>
-                                            <p>Let your app stand apart with some animation.  A nice way for visitors to interact with your site.</p>
-                                            <p>Just one of the <strong>extra goodies</strong> included with Scribe.</p>
-                                        </span>
-                                        <nav>
-                                            <a href="#carousel-example-generic" data-slide="prev"><span class="fa fa-chevron-circle-left" aria-hidden="true"></span></a>
-                                            <a href="#carousel-example-generic" data-slide="next"><span class="fa fa-chevron-circle-right" aria-hidden="true"></span></a>
-                                        </nav>
-                                    </div>
+                        </div>
+                        <!-- SUPPORT -->
+                        <div class="sc_stats col-md-3">
+                            <i class="fa fa-life-ring" aria-hidden="true"></i>
+                            <div>
+                                <span class="highlight">365</span>
+                                <div class="milestone-details">
+                                    <p>Support Requests</p>
                                 </div>
                             </div>
-                            <!-- Controls -->
+                        </div>
+                        <div class="col-md-6">
+                            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Donec id elit non mi porta gravida at eget metus. </p>
+                        </div>
+                        <div class="col-md-6">
+                            <!-- MOBILE -->
+                            <div class="sc-feature-box text-center col-md-6">
+                                <a href="">
+                                    <i class="hi-icon fa fa-mobile" aria-hidden="true"></i>
+                                </a>
+                                <h4>800-123-4321</h4>
+                            </div>
+                            <!-- MAIL -->
+                            <div class="sc-feature-box text-center col-md-6">
+                                <a href="">
+                                    <i class="hi-icon fa fa-envelope-o" aria-hidden="true"></i>
+                                </a>
+                                <h4>scribe@email.com</h4>
+                            </div>
                         </div>
                     </div>
                 </div>
+                <!-- END CONTENT -->
                 <script src="js/bootstrap.min.js" type="text/javascript"></script>
             </div>
+        </div>
     </body>
 </html>
